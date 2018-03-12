359f539a593788f825d077ed38c6e751ad62f98e
李育陞 <e1018e1018@gmail.com>
2018-03-12 17:14:09 +0800

    20180312

---
+++ NTTU_Lab2/NTTU_Lab2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NTTU_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\admin\Desktop\lab-2-zi-chuan-chu-li-e1018e1018\Data\Input.txt");
+            String line;
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                var chars = line.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] == '.' || chars[i] == '?' || chars[i] == '!')
+                    {
+                        Console.WriteLine(chars[i]);
+                    }
+                    else
+                    {
+                        Console.Write(chars[i]);
+                    }              
+                }
+            }
+            Console.Read();  
+        }
+    }
+}
